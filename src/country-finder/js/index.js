acd87c6a3ce2f2e03b171c37e77c6190b37f5349
Ivan Mutsko <imucko1996@gmail.com>
2023-03-05 21:58:27 +0200

    add country finder

--- src/country-finder/js/index.js
+++ src/country-finder/js/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
